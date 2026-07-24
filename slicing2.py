118bdc8794169678ccb6ab3c77bec9a5344ad99d
NikitaAmbre <nikitaambre57@gmail.com>
2026-07-24 11:53:25 +0530

    Updated project files

---
+++ slicing2.py
@@ -0,0 +1,29 @@
+v = 'DataScienceWithPython'
+print(v[4:11])
+
+p = 'ArtificialIntelligence'
+print(p[1::3])
+
+s = 'MachineLearning'
+print(s[:-9:-1])
+
+fruits = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape', 'kiwi']
+print(fruits[2:6])
+
+numbers = [5, 10, 15, 20, 25, 30, 35, 40]
+print(numbers[1::2])
+
+colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+print(colors[3:]+colors[:3])
+
+m = (2, 4, 6, 8, 10, 12, 14, 16)
+print(m[2::2])
+
+t= ('a', 'b', 'c', 'd', 'e', 'f', 'g')
+print(t[::-2])
+
+matrix = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+print(matrix[-2:])
+
+data = [('John', 25), ('Emma', 30), ('Alex', 28), ('Sophia', 32), ('Liam', 27)]
+print(data[1:4])
