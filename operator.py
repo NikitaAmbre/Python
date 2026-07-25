ceab3449769e0d06073b1969f211d1a99a120542
NikitaAmbre <nikitaambre57@gmail.com>
2026-07-25 22:44:07 +0530

    homepage layout fix

--- operator.py
+++ operator.py
@@ -1,59 +1,58 @@
-#arithmetic operator: it is used to perform arithemtic logic or operations
+#operators
+# 1. Arithmetic Operators(+,_,*,%,/)
+num1 = 40
+num2 = 8
+print(num1 - num2)       # subtraction
+print(num1 * num2)       # multiplication
+print(num1 + num2)       # addition  ( we also use + to concatenate 2 strings)
+print(num1 / num2)       # division  
+print(num1 % num2)       # modulus (returns remainder)   
 
-#additional operator
-a=10
-b=20
-print(a+b)
+#2. Assignment Operators(=)
+value = 50               # assign 
+print(value)
+value -= 10              # subtract and assign
+print(value)
 
-# we use + operator as concatinate 2 strings,list,tuple
+# 3. Comparison Operators(>,<,<=,>=,!=,==)
+p3 = 30
+p4 = 25
+print(p3<p4)    # less than comparison
+print(p3 > p4)  # greater than comparison
+print(p3>=p4)   #greater than equal to
+print(p3<=p4)   # less than equal to
+print(p3==p4)   #exactly equal to or include equal
+print(p3!=p4)   # not equal to 
 
-a1=[1,2,3]+[4,5,6]
-print(a1)
+# 4. Logical Operators (and,or,not)
+x1 = 5
+x2 = 15
+x3 = 25
+x4 = 35
+print("And result :", x1 < x2 and x3 > x4)   # AND condition
+print("OR Result :", x1 < x2 or x3 > x4)     # OR condition
+print("not Result :", not x3 > x4)           # NOT condition
 
-a2='hello'+'world'
-print(a2)
+# 5. Identity Operators (is, is not)
+m1 = 100
+m2 = 100
+print(m1 is m2)
+print(m1 is not m2)
 
-#substraction operator
-print(a-b)
+# 6. Membership Operators (in, not in)
+print("Check 'a' in 'mango' :", 'a' in 'mango')
+print("Check 'a' in 'mango' :", 'a' not in 'mango')
 
-# multiplication 
-
-b1=['nikita']*3
-print(b1)
-
-s=10.8
-p=2
-print(s*p)
-
-
-#Logical operator: it will perform basic logical operations between 2 values/datatypes.
-
-# There are 3 types:
-
-#     Logical AND (and): op1 and op2
-#     Logical OR (or): op1 or op2
-#     Logical NOT(not): not(op)
+# 7. Bitwise Operators. (AND(&), OR(|), NOT(~),XOR(^), Left Shift(<<),Right shift(>>))
+a=10                #binary:1010
+b=4                 #binary:0100
+result=a & b        # Bitwise AND (&)
+result=a | b        # Bitwise OR (|)
+result= ~(a + b)    # Bitwise NOT (~)   
+result=a ^ b        # Bitwise XOR (^)
+result=a << b       # Bitwise Left shift (<<)
+result=a >> b       # Bitwise Right shift (>>)
+print(result)
 
 
-# Assignment Operator: it is used to assign values in a variable
-
-# '=' is the assignment operator
-
-# Eg: b=12.34
-
-# Membership Operator: it will check if a value is present in the collection.
-
-# in: val in collection
-
-# Eg: 10 in [10,20,30,40,50]
-
-# True
-
-# Eg: 70 in [10,20,30,40,50]
-
-# False
-
-# not in: val not in collection Eg: 10 not in [10,20,30,40,50] False
-
-# 100 not in [10,20,30,40,50] True
 
